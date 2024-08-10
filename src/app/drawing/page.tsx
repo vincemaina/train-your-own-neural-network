1e6cbfe039fe9b36682dcb33596752d0a55b2fcc
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-10 11:44:20 +0100

    feat: add better styling and layout

--- src/app/drawing/page.tsx
+++ src/app/drawing/page.tsx
@@ -4,9 +4,14 @@ import { Canvas } from "@/components/drawing/canvas";
 export default function Page() {
     return (
         <>
-            <h1>Drawing</h1>
-            <div className="flex gap-2">
+            <div className="text-center mb-5">
+                <h1>User Input Processing</h1>
+                <p>This page demonstrates how the application takes user inputs (i.e. hand-drawn numbers), and processes them into numerical data that we can pass into the input layer of our neural network.</p>
+            </div>
+            <div className="grid grid-cols-2 grid-rows-2 gap-5">
                 <Canvas/>
+                <AnalysedCanvas/>
+                <AnalysedCanvas/>
                 <AnalysedCanvas/>
             </div>
         </>
