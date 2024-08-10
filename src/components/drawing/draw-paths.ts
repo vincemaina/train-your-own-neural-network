043c49376e6b9e03652a572347005e3c7fb680cf
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-10 12:05:24 +0100

    feat: add function for drawing input path on input canvas

---
+++ src/components/drawing/draw-paths.ts
@@ -0,0 +1,32 @@
+import { Path } from "./canvas";
+
+export function drawPaths(canvas: HTMLCanvasElement, paths: Path[]): void {
+    console.log('Drawing paths');
+    
+    // Get the context
+    const context = canvas.getContext('2d');
+    if (!context) {
+        console.log('No context');
+        return;
+    }
+    
+    // Clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Redraw each path
+    context.beginPath();
+    context.lineWidth = 20;
+    context.lineCap = 'round';
+    context.strokeStyle = 'black';
+    context.imageSmoothingEnabled = true;
+    paths.forEach((path) => {
+        path.forEach((point, index) => {
+            if (index === 0) {
+                context.moveTo(point.x, point.y);
+            } else {
+                context.lineTo(point.x, point.y);
+            }
+        });
+    });
+    context.stroke();
+}
