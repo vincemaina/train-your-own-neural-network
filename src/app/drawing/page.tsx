af36f16e6958b805f57d39c037aaa820f1d9d28a
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-11 01:48:02 +0100

    refactor: modify text

--- src/app/drawing/page.tsx
+++ src/app/drawing/page.tsx
@@ -10,13 +10,10 @@ export default function Page() {
     return (
         <div className="flex flex-col text-white">
             <div className="text-center mb-10 container mx-auto">
-                <h1>User Input Processing</h1>
+                <h1>Train Your Own Neural Network</h1>
                 <p>
-                    This page demonstrates how the application takes user inputs (i.e. hand-drawn numbers),
-                    and processes them into numerical data that we can pass into the input layer of our neural network.
-                    <span className="font-semibold text-blue-400"> Start by drawing a number in the first square. </span>
-                    The second square normalises your drawing (i.e. scales it to fit the square), and the third square rasterizes the drawing (i.e. converts it to a 28x28 grid of binary pixels).
-                    The fourth square displays these binary pixels as a flattened array of 0s and 1s i.e. the input data for our neural network.
+                    This tools allows you to train a simple neural network from scratch to recognize handwritten characters.
+                    <span className="font-semibold text-blue-400"> Start by drawing a character in the first square. </span>
                 </p>
             </div>
 
